7fe6d4b16c4a0cf1b558c71b945b0a879dff064b
@Eva-87 <mariaeva_martin@hotmail.com>
2026-02-06 14:19:01 +0100

    fix the model Book.java

--- src/main/java/com/femteca/model/Book.java
+++ src/main/java/com/femteca/model/Book.java
@@ -1,25 +1,50 @@
 package com.femteca.model;
 
 public class Book {
+
+    private int id;
     private String title;
     private String description;
     private String code;
 
-    public Book(String title, String description, String code) {
+    public Book (String title, String description, String code) {
+    
         this.title = title;
         this.description = description;
         this.code = code;
+
+    }
+    public Book () {
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
     public String getTitle() {
         return title;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     public String getDescription() {
         return description;
     }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
     public String getCode() {
         return code;
     }
+    
+
 }
