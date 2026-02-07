58b811d18b7ef261838c4a515ec3bd319512c1d8
ManonChab <godfroymanon@gmail.com>
2026-02-07 11:43:41 +0100

    fix :recreated BookRepository to fix the issue

---
+++ src/main/java/com/femteca/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.femteca.repository;
+
+import com.femteca.model.Book;
+
+public interface BookRepository {
+
+    void createBook(Book book);
+
+    Book readBookById(int id);
+    
+    void updateBook(Book book);
+
+    void deleteBook(int id);
+    }
