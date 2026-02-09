1789636b6bb91405e492117b419b30d63ddde4f2
ManonChab <godfroymanon@gmail.com>
2026-02-09 10:29:02 +0100

    feature : started tests

--- src/test/java/com/femteca/AppTest.java
+++ src/test/java/com/femteca/AppTest.java
@@ -3,18 +3,44 @@ package com.femteca;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+import com.femteca.model.Book;
+import com.femteca.repository.BookRepositoryImpl;
+
+@ExtendWith(MockitoExtension.class)
+class BookRepositoryImplTest {
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private BookRepositoryImpl bookRepository;
+
+@Test
+void shouldCallAuthorRepositoryWhenLoadingBook() {
+
+    int authorId = 2;
+
+    Author author = new Author();
+    author.setId(authorId);
+    author.setName("Isabel Allende");
+
+    when(authorRepository.findById(authorId)).thenReturn(author);
+
+    // simulate BookRepository logic
+    Book book = new Book(
+        "La casa de los espíritus",
+        "Realismo mágico",
+        "ISBN-456",
+        author
+    );
+
+    // verify
+    verify(authorRepository, times(1)).findById(authorId);
+    assertEquals("Isabel Allende", book.getAuthor().getName());
+}
 }
