fbc5c43bfdd00f9a9cb73093d1c73ad175a6e46e
@Eva-87 <mariaeva_martin@hotmail.com>
2026-02-10 12:48:20 +0100

    ready to download dev to my local repo

---
+++ src/main/java/com/femteca/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.femteca.repository;
+
+import com.femteca.model.Book;
+
+public interface BookRepository {
+
+    void createBook(Book book);
+
+    Book readBookById(int id);
+    
+     void updateBook(Book book);
+
+    void deleteBook(int id);
+    }
+
